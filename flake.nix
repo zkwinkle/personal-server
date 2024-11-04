fbc3f52f5f2da448695105094c8f428f2d99aa09
Ignacio Vargas <ignaevc@gmail.com>
2024-11-03 18:48:20 -0600

    auto format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,24 +5,24 @@
   };
 
   outputs = { self, nixpkgs, website, uwgpu }:
-  let
-  system = "x86_64-linux";
-  pkgs = import nixpkgs {
-    inherit system;
-    overlays = [
-      (
-        final: prev: {
-          uwgpu-server = uwgpu.packages.${prev.system}.web-server;
-          personal-website = website.packages.${prev.system}.default;
-        }
-      )
-    ];
-  };
-  in
-  {
-  nixosConfigurations.personal-server = nixpkgs.lib.nixosSystem {
-    inherit pkgs system;
-    modules = [ ./configuration ];
-  };
-};
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (
+            final: prev: {
+              uwgpu-server = uwgpu.packages.${prev.system}.web-server;
+              personal-website = website.packages.${prev.system}.default;
+            }
+          )
+        ];
+      };
+    in
+    {
+      nixosConfigurations.personal-server = nixpkgs.lib.nixosSystem {
+        inherit pkgs system;
+        modules = [ ./configuration ];
+      };
+    };
 }
